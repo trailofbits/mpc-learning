a72b363e1f245f3bb08f58a085a001d2539df669
James Miller <jamesmiller@Jamess-MacBook-Pro.local>
2019-06-28 15:02:08 -0400

    requirements.txt added, setup.py requirements added

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="MPC-learning",
     version="1.0",
+    install_requires=requirements,
     packages=['src','examples','data'],
     long_description=open("README.md").read(),
     platforms=['any']
